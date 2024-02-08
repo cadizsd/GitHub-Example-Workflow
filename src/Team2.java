fec4c0f9a66acd24ede8dc207ae504a530437db1
brian <demyanb@moravian.edu>
2024-02-08 10:40:57 -0500

    adding team member

--- src/Team2.java
+++ src/Team2.java
@@ -17,14 +17,9 @@ public class Team2 extends Team
     public void createTeamMembers()
     {
         members.add(new TeamMember("Samuel Cadiz"));
-
         members.add(new TeamMember("Deni Velasquez"));
-        members.add(new TeamMember(""));
-
-        members.add(new TeamMember(""));
         members.add(new TeamMember("Kaleb Missmer"));
-
-        members.add(new TeamMember(""));
+        members.add(new TeamMember("Brian Demyan"));
         members.add(new TeamMember("Fernando Peralta Castro"));
         // create a new member class for each team member
         // and add it to the team members list
