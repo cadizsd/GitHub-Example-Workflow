dd0d1ffc4bac5d0c686e72cf9570949378bc9487
Samuel Cadiz <cadizs@moravian.edu>
2024-02-08 09:56:02 -0500

    New team added , add names

---
+++ src/Team2.java
@@ -0,0 +1,30 @@
+package Project00;
+import java.util.*;
+import Project00.Team;
+import Project00.Member;
+import Project00.TeamMember;
+
+
+
+public class Team2 extends Team
+{
+    public Team2(String name)
+    {
+        super(name);
+        createTeamMembers();
+    }
+
+    public void createTeamMembers()
+    {
+        members.add(new TeamMember("Samuel Cadiz"));
+        members.add(new TeamMember(""));
+        members.add(new TeamMember(""));
+        members.add(new TeamMember(""));
+        members.add(new TeamMember(""));
+        // create a new member class for each team member
+        // and add it to the team members list
+    }
+}
+
+
+
